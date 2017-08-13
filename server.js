56d8800f781de244e391ee13c9276502ab858926
Aimee-MacDonald <blibzblu@gmail.com>
2017-08-14 00:06:23 +0200

    first final

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require("express");
 var app = express();
 var portNumber = 8000;
 
+app.get("/", function(req, res){
+  var userIP = req.ips;
+  var userLang = req.headers["accept-language"];
+  var userOS = req.headers["user-agent"];
+
+  var respac = {
+    ipaddress: userIP,
+    language: userLang,
+    software: userOS
+  };
+
+  res.status(200).send(respac);
+});
+
 app.listen(portNumber, function(){
   console.log("Server Listening on Port: " + portNumber);
 });
